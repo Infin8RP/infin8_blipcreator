34e091d7652fa0f336bb1653dc648be9a6af8cb0
jonnie-w <113666202+jonnie-w@users.noreply.github.com>
2026-05-17 00:12:48 +0100

    [c] moved webhook server side to enhance security

--- config.lua
+++ config.lua
@@ -12,5 +12,3 @@ Config.Storage = {
     Type = 'json',
     FileName = 'blips.json'
 }
-
-Config.Webhook = 'https://discord.com/api/webhooks/1505189570524872704/GYQE6BSVhv0xoEfdGd4JCrSrHU0lv7HCbsE-9SLx2fR7Y8FC_fklZKhpvIHXdy5XGRRx'
